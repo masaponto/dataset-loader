17aaa0edbc5edc206eea5cc020ba27c2df62d933
masaponto <masaponto@gmail.com>
2016-03-31 20:25:27 +0900

    [FIX]

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='dataset-loader',
+    name='dataset_loader',
     version='1.0',
     description='machine learning dataset loader for my experiment',
     author='masaponto',
     author_email='masaponto@gmail.com',
     url='masaponto.github.io',
     install_requires=['numpy', 'scikit-learn'],
-    py_modules = ["dataset-loader"],
+    py_modules = ["dataset_loader"],
     package_dir = {'': 'src'}
 )
