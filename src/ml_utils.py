68bb9c7178b60d5a421ba50a358ac28b5b57ca7b
masaponto <masaponto@gmail.com>
2016-12-26 22:33:32 +0900

    [Add] add scaling method

--- src/ml_utils.py
+++ src/ml_utils.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import numpy as np
+from sklearn.utils import shuffle
+from sklearn.preprocessing import StandardScaler
 
 
-def cross_validation(estimator, data_set, k=5):
+def cross_validation(estimator, data_set, k=5, scaling=False):
     '''
     find cross_validation accuracy
 
@@ -11,19 +13,17 @@ def cross_validation(estimator, data_set, k=5):
 
     '''
 
-    from sklearn.utils import shuffle
-
     X, y = shuffle(data_set.data, data_set.target)
 
     n = data_set.data.shape[0]
     m = n // k
 
-    scores = [validation(estimator, X, y, m, index) for index in range(0, n - (n % k), m)]
+    scores = [validation(estimator, X, y, m, index, scaling) for index in range(0, n - (n % k), m)]
 
     return np.array(scores)
 
 
-def validation(estimator, X, y, m, index):
+def validation(estimator, X, y, m, index, scaling=False):
     x_test = X[index: index + m]
     y_test = y[index: index + m]
 
@@ -36,6 +36,11 @@ def validation(estimator, X, y, m, index):
     x_train = np.r_[x_train1, x_train2]
     y_train = np.r_[y_train1, y_train2]
 
+    if scaling:
+        scaler = StandardScaler().fit(x_train)
+        x_train = scaler.transform(x_train)
+        x_test = scaler.transform(x_test)
+
     estimator.fit(x_train, y_train)
     return estimator.score(x_test, y_test)
 
@@ -44,8 +49,7 @@ def argwrapper(args):
     return args[0](*args[1:])
 
 
-def mp_cross_validation(estimator, data_set, k=5, p_num=4):
-    from sklearn.utils import shuffle
+def mp_cross_validation(estimator, data_set, k=5, p_num=4, scaling=False):
     from multiprocessing import Pool
     from multiprocessing import Process
 
@@ -55,7 +59,7 @@ def mp_cross_validation(estimator, data_set, k=5, p_num=4):
     n = n - (n % k)
 
     p = Pool(p_num)
-    func_args = [(validation, estimator, data_set.data, data_set.target, m, index) for index in range(0, n, m)]
+    func_args = [(validation, estimator, data_set.data, data_set.target, m, index, scaling) for index in range(0, n, m)]
     scores = p.map(argwrapper, func_args)
 
     return np.array(scores)
@@ -67,10 +71,10 @@ def main():
     from sklearn.datasets import fetch_mldata
 
     data_set = fetch_mldata('australian')
-    data_set.data = normalize(data_set.data)
+    #data_set.data = normalize(data_set.data)
 
-    print(mp_cross_validation(ELM(100), data_set))
-    print(cross_validation(ELM(100), data_set))
+    print(mp_cross_validation(ELM(100), data_set, scaling=True))
+    print(cross_validation(ELM(100), data_set, scaling=True))
 
 
 if __name__ == "__main__":
