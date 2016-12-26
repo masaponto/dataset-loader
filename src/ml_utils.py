f9e7e5900470b588990f987a4a0ba4ff32dbd9b3
masaponto <masaponto@gmail.com>
2016-12-26 16:08:12 +0900

    [Add] add multi processing

--- src/ml_utils.py
+++ src/ml_utils.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def cross_validation(estimator, data_set, cv=5):
+def cross_validation(estimator, data_set, k=5):
     '''
     find cross_validation accuracy
 
@@ -16,24 +16,47 @@ def cross_validation(estimator, data_set, cv=5):
     X, y = shuffle(data_set.data, data_set.target)
 
     n = data_set.data.shape[0]
-    k = n // cv
-    scores = []
+    m = n // k
 
-    for index in range(0, n - (n % cv), k):
-        x_test = X[index: index + k]
-        y_test = y[index: index + k]
+    scores = [validation(estimator, X, y, m, index) for index in range(0, n - (n % k), m)]
 
-        x_train1 = X[:index]
-        y_train1 = y[:index]
+    return np.array(scores)
 
-        x_train2 = X[index + k:]
-        y_train2 = y[index + k:]
 
-        x_train = np.r_[x_train1, x_train2]
-        y_train = np.r_[y_train1, y_train2]
+def validation(estimator, X, y, m, index):
+    x_test = X[index: index + m]
+    y_test = y[index: index + m]
 
-        estimator.fit(x_train, y_train)
-        scores.append(estimator.score(x_test, y_test))
+    x_train1 = X[:index]
+    y_train1 = y[:index]
+
+    x_train2 = X[index + m:]
+    y_train2 = y[index + m:]
+
+    x_train = np.r_[x_train1, x_train2]
+    y_train = np.r_[y_train1, y_train2]
+
+    estimator.fit(x_train, y_train)
+    return estimator.score(x_test, y_test)
+
+
+def argwrapper(args):
+    return args[0](*args[1:])
+
+
+def mp_cross_validation(estimator, data_set, k=5, p_num=4):
+    from sklearn.utils import shuffle
+    from multiprocessing import Pool
+    from multiprocessing import Process
+
+    X, y = shuffle(data_set.data, data_set.target)
+    n = data_set.data.shape[0]
+    m = n // k
+    n = n - (n % k)
+
+    p = Pool(p_num)
+    func_args = [(validation, estimator, data_set.data, data_set.target, m, index) for index in range(0, n, m)]
+    scores = p.map(argwrapper, func_args)
 
     return np.array(scores)
 
@@ -46,7 +69,8 @@ def main():
     data_set = fetch_mldata('australian')
     data_set.data = normalize(data_set.data)
 
-    print(cross_validation(ELM(100), data_set).mean())
+    print(mp_cross_validation(ELM(100), data_set))
+    print(cross_validation(ELM(100), data_set))
 
 
 if __name__ == "__main__":
